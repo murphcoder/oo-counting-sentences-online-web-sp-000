9a8a1656f2277e5267613876b5d715009f5f1e38
Sean Murphy <filmmakersean@fastmail.net>
2019-07-13 00:06:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,7 @@ class String
   end
 
   def count_sentences
-    sentences = []
-    self.split(/[.?!]/) = sentences
+    sentences = self.split(/[.?!]/)
     sentences.reject!(empty)
     sentences.count
   end
